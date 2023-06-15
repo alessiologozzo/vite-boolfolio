63bfb9e002b73212cf36750f470379c7cf6fa8df
alessiologozzo <alessio.logozzo@libero.it>
2023-06-15 16:40:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+<AppMain/>
+
+</template>
+
+<script>
+import axios from "axios";
+import { RouterLink, RouterView } from 'vue-router'
+import AppMain from './components/AppMain.vue'
+
+  export default {
+    name: "App",
+
+    components: {
+      AppMain
+    },
+
+    methods: {
+        queryServer() {
+
+
+
+            axios.get("http://127.0.0.1:8000/api/").then((response) => {
+                console.log(response.data);
+            });
+
+        },
+    },
+
+    mounted() {
+        this.queryServer();
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
